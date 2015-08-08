40b37ac0f15d504ca23315065b554df77727d177
Jirawong Wongdokpuang <jirawong@linksinnovation.com>
2015-08-09 03:55:48 +0700

    query method and jpql

--- src/main/java/com/linksinnovation/springboot/controller/HomeRestController.java
+++ src/main/java/com/linksinnovation/springboot/controller/HomeRestController.java
@@ -1,7 +1,11 @@
 package com.linksinnovation.springboot.controller;
 
 import com.linksinnovation.springboot.domain.Comment;
+import com.linksinnovation.springboot.repository.CommentRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,21 +18,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HomeRestController {
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public Comment home(){
-        Comment comment = new Comment();
-        comment.setComment("this is comment");
-        comment.setAuthor("this is author");
-        return comment;
-    }
-    
-    @RequestMapping(method = RequestMethod.POST)
-    public Comment save(@Validated @RequestBody Comment comment ){
-        
 
-        
-        return comment;
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Comment> listAll() {
+        return commentRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Comment getOne(@PathVariable("id") Integer id) {
+        return commentRepository.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Comment save(@Validated @RequestBody Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public Comment update(@Validated @RequestBody Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public List<Comment> delete(@PathVariable("id") Integer id) {
+        Comment findOne = commentRepository.findOne(id);
+        commentRepository.delete(findOne);
+        return commentRepository.findAll();
     }
     
+    @RequestMapping(value = "/author/{author}",method = RequestMethod.GET)
+    public List<Comment> findAuthor(@PathVariable("author") String author){
+        return commentRepository.findByAuthor(author);
+    }
+    
+    @RequestMapping(value = "/author/search/{author}",method = RequestMethod.GET)
+    public List<Comment> searchAuthor(@PathVariable("author") String author){
+        return commentRepository.findByAuthorLike("%"+author+"%");
+    }
+    
+    @RequestMapping(value = "/comment/search/{comment}",method = RequestMethod.GET)
+    public List<Comment> searchComment(@PathVariable("comment") String comment){
+        return commentRepository.searchComment(comment);
+    }
+
 }
--- src/main/java/com/linksinnovation/springboot/repository/CommentRepository.java
+++ src/main/java/com/linksinnovation/springboot/repository/CommentRepository.java
@@ -6,12 +6,17 @@
 package com.linksinnovation.springboot.repository;
 
 import com.linksinnovation.springboot.domain.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author Jirawong Wongdokpuang <jirawong@linksinnovation.com>
  */
 public interface CommentRepository extends JpaRepository<Comment, Integer>{
-    
+    public List<Comment> findByAuthor(String author);
+    public List<Comment> findByAuthorLike(String author);
+    @Query("SELECT c FROM Comment c WHERE c.comment LIKE %?1%")
+    public List<Comment> searchComment(String comment);
 }
