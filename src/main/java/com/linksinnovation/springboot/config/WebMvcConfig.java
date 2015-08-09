43da53162f4228364156cbadbcb761d502dae941
Jirawong Wongdokpuang <jirawong@linksinnovation.com>
2015-08-09 14:18:21 +0700

    add custome form

---
+++ src/main/java/com/linksinnovation/springboot/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.linksinnovation.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Jirawong Wongdokpuang <greannetwork@gmail.com>
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+    
+    
+    
+}
